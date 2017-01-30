4d06ad8f968bbbbf45e7e568bb77c6a32650dbb2
Eunseok Eom <me@eseom.org>
2017-01-31 02:45:32 +0900

    enhance User model and add test to that

---
+++ src/server/__tests__/models-user-test.js
@@ -0,0 +1,36 @@
+// @flow
+
+import { expect } from 'chai';
+import { User, sequelize } from '../models';
+import logger from '../../logger';
+
+before(async (done) => {
+  await sequelize.sync({ force: true });
+  done(null);
+});
+
+describe('UserTest', () => {
+  let u;
+
+  beforeEach(() => {
+    u = User.build({
+      username: 'example-user',
+      password: 'foobar',
+      passwordConfirmation: 'foobar',
+    });
+  });
+
+  it('save an user', async () => {
+    const user = await u.save();
+    expect(user.username).equal('example-user');
+  });
+
+  it('get the saved user', async () => {
+    const user = await User.find({
+      username: 'example-user',
+    });
+    expect(user.username).equal('example-user');
+    expect(user.authenticate('foobar2')).equal(false);
+    expect(user.authenticate('foobar')).equal(true);
+  });
+});
--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,13 +1,53 @@
 // @flow
 
+import bcrypt from 'bcrypt';
+
+const hasSecurePassword = (user, options, callback) => {
+  if (user.password !== user.passwordConfirmation) {
+    throw new Error("Password confirmation doesn't match Password");
+  }
+  bcrypt.hash(user.get('password'), 10, (err, hash) => {
+    if (err) return callback(err);
+    user.set('passwordDigest', hash);
+    return callback(null, options);
+  });
+};
+
 export default (sequelize, DataTypes) => {
-  const { INTEGER, STRING, DATE } = DataTypes;
+  const { INTEGER, STRING, VIRTUAL, DATE } = DataTypes;
+
+  // https://nodeontrain.xyz/tuts/secure_password/
   const User = sequelize.define('users', {
     id: { type: INTEGER, primaryKey: true, autoIncrement: true, scopes: ['public'] },
     username: STRING,
+    passwordDigest: {
+      field: 'password',
+      type: STRING,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    password: {
+      field: 'passwordVirtual',
+      type: VIRTUAL,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    passwordConfirmation: {
+      type: VIRTUAL,
+    },
     createdAt: { type: DATE, field: 'created_at' },
     updatedAt: { type: DATE, field: 'updated_at' },
   }, {
+    indexes: [{ unique: true, fields: ['username'] }],
+    instanceMethods: {
+      authenticate: function authenticate(value) {
+        if (bcrypt.compareSync(value, this.passwordDigest)) return true;
+        return false;
+      },
+    },
     classMethods: {
       associate: (models) => {
         User.hasMany(models.Todo);
@@ -16,6 +56,19 @@ export default (sequelize, DataTypes) => {
     createdAt: 'createdAt',
     updatedAt: 'updatedAt',
   });
+  User.beforeCreate((user, options, callback) => {
+    user.username = user.username.toLowerCase();
+    if (user.password) {
+      return hasSecurePassword(user, options, callback);
+    }
+    return callback(null, options);
+  });
+  User.beforeUpdate((user, options, callback) => {
+    user.username = user.username.toLowerCase();
+    if (user.password) hasSecurePassword(user, options, callback);
+    return callback(null, options);
+  });
+
   return {
     User,
   };
